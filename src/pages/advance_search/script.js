ffd85655fafbb6c56f0570cd0ffb9358c5ddd645
Muhammad Saqib <mohammadsaqib1997@gmail.com>
2018-04-28 20:31:45 +0500

    Complete Advance Search

--- src/pages/advance_search/script.js
+++ src/pages/advance_search/script.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase'
 import _ from 'lodash'
+import moment from 'moment'
 
 import tableComp from '../../partials/components/html_utils/tabel_comp.vue'
 
@@ -13,6 +14,9 @@ export default {
         self.mounted_async();
     },
     methods: {
+        dateFormat(ms) {
+            return moment(ms).format("hh:mm A, DD/MM/YYYY")
+        },
         async mounted_async() {
             const self = this
 
@@ -23,10 +27,19 @@ export default {
             }))
             self.asyncData.addaListData = grabAdda
         },
-        async search_table() {
+        async search_users() {
+            const self = this
             const db = firebase.database();
-            const self = this;
+
             let dataSnap = await db.ref(self.sel_table).once('value')
+
+            let grab_map_data = []
+            await Promise.all(_.map(dataSnap.val(), async (row, key) => {
+                let render = row
+                render['id'] = key
+
+                grab_map_data.push(render)
+            }))
 
             let grabSearchField = {}
             if (self.table_list[self.sel_table].fields) {
@@ -36,12 +49,13 @@ export default {
                     }
                 }
             }
-            let grabFilterData = await _.filter(dataSnap.val(), grabSearchField);
+            let grabFilterData = await _.filter(grab_map_data, grabSearchField);
 
             let tableSet = {
                 thead: [],
                 tbody: grabFilterData
             }
+            // Data template
             if (self.table_list[self.sel_table].data_template) {
                 for (let d_temp_val of self.table_list[self.sel_table].data_template) {
                     tableSet['thead'].push(d_temp_val.thead)
@@ -51,25 +65,146 @@ export default {
                 await Promise.all(_.map(grabFilterData, async (row, key) => {
                     let item = {}
                     await Promise.all(_.map(self.table_list[self.sel_table].data_template, async (d_temp_val) => {
-                        item[d_temp_val.field] = row[d_temp_val.field]
-                        if (d_temp_val.hasOwnProperty('ref')) {
-                            let adda = _.find(self.asyncData[d_temp_val.ref], {value: row[d_temp_val.field]})
-                            if(adda) {
-                                item[d_temp_val.field] = adda.title
+                        if (d_temp_val.hasOwnProperty('field')) {
+                            item[d_temp_val.field] = row[d_temp_val.field]
+                            if (d_temp_val.hasOwnProperty('ref')) {
+                                let adda = _.find(self.asyncData[d_temp_val.ref], { value: row[d_temp_val.field] })
+                                if (adda) {
+                                    item[d_temp_val.field] = adda.title
+                                }
                             }
                         }
                     }))
                     mapData.push(item)
                 }))
 
+                tableSet['tbody'] = mapData
+            }
+
+            self.searchResult = tableSet
+            self.searchHit = true
+
+        },
+        async search_requests() {
+            const self = this
+            const db = firebase.database();
+
+            let dataSnap = await db.ref(self.sel_table).once('value')
+
+            let grab_map_data = []
+            await Promise.all(_.map(dataSnap.val(), async (row_1, key_1) => {
+                await Promise.all(_.map(row_1, async (row_2) => {
+                    let render = row_2
+                    render['uid'] = key_1
+                    render['createdAt'] = self.dateFormat(row_2.createdAt)
+                    render['canceledAt'] = (row_2.canceledAt) ? self.dateFormat(row_2.canceledAt) : ''
+
+                    grab_map_data.push(render)
+                }))
+            }))
+            let grabSearchField = {}
+            if (self.table_list[self.sel_table].fields) {
+                for (let field of self.table_list[self.sel_table].fields) {
+                    if (field.sel_value !== "") {
+                        grabSearchField[field.id] = field.sel_value
+                    }
+                }
+            }
+            let grabFilterData = await _.filter(grab_map_data, grabSearchField);
+
+            let tableSet = {
+                thead: [],
+                tbody: grabFilterData
+            }
+            // Data template
+            if (self.table_list[self.sel_table].data_template) {
+                for (let d_temp_val of self.table_list[self.sel_table].data_template) {
+                    tableSet['thead'].push(d_temp_val.thead)
+                }
+
+                let mapData = []
+                await Promise.all(_.map(grabFilterData, async (row, key) => {
+                    let item = {}
+                    await Promise.all(_.map(self.table_list[self.sel_table].data_template, async (d_temp_val) => {
+                        if (d_temp_val.hasOwnProperty('field')) {
+                            item[d_temp_val.field] = row[d_temp_val.field]
+                        }
+                    }))
+                    mapData.push(item)
+                }))
 
                 tableSet['tbody'] = mapData
             }
 
+            self.searchResult = tableSet
+            self.searchHit = true
 
+        },
+        async search_bids() {
+            const self = this
+            const db = firebase.database();
+
+            let dataSnap = await db.ref(self.sel_table).once('value')
+            
+            let grab_map_data = []
+            await Promise.all(_.map(dataSnap.val(), async (row_1, key_1) => {
+                await Promise.all(_.map(row_1, async (row_2, key_2) => {
+                    let render = row_2
+                    render['req_id'] = key_1
+                    render['driver_id'] = key_2
+                    render['first_bid_time'] = self.dateFormat(row_2.first_bid_time)
+
+                    grab_map_data.push(render)
+                }))
+            }))
+
+            let grabSearchField = {}
+            if (self.table_list[self.sel_table].fields) {
+                for (let field of self.table_list[self.sel_table].fields) {
+                    if (field.sel_value !== "") {
+                        grabSearchField[field.id] = field.sel_value
+                    }
+                }
+            }
+            let grabFilterData = await _.filter(grab_map_data, grabSearchField);
+            
+            let tableSet = {
+                thead: [],
+                tbody: grabFilterData
+            }
+            // Data template
+            if (self.table_list[self.sel_table].data_template) {
+                for (let d_temp_val of self.table_list[self.sel_table].data_template) {
+                    tableSet['thead'].push(d_temp_val.thead)
+                }
+
+                let mapData = []
+                await Promise.all(_.map(grabFilterData, async (row, key) => {
+                    let item = {}
+                    await Promise.all(_.map(self.table_list[self.sel_table].data_template, async (d_temp_val) => {
+                        if (d_temp_val.hasOwnProperty('field')) {
+                            item[d_temp_val.field] = row[d_temp_val.field]
+                        }
+                    }))
+                    mapData.push(item)
+                }))
+
+                tableSet['tbody'] = mapData
+            }
 
             self.searchResult = tableSet
             self.searchHit = true
+            
+        },
+        async search_table() {
+            const self = this;
+            if (self.sel_table === "users") {
+                self.search_users()
+            } else if (self.sel_table === "user_requests") {
+                self.search_requests()
+            } else if (self.sel_table === "driver_bids") {
+                self.search_bids()
+            }
         }
     },
     data() {
@@ -118,6 +253,16 @@ export default {
                             ]
                         },
                         {
+                            label: 'Select Offline Status',
+                            tag_type: 'select',
+                            id: 'offline',
+                            sel_value: '',
+                            options: [
+                                { value: true, title: 'Offline Driver' },
+                                { value: false, title: 'Online Driver' }
+                            ]
+                        },
+                        {
                             label: 'Select Adda',
                             tag_type: 'select',
                             id: 'adda_ref',
@@ -126,6 +271,13 @@ export default {
                         }
                     ],
                     data_template: [
+                        {
+                            thead: 'S.NO#'
+                        },
+                        {
+                            thead: 'ID',
+                            field: 'id'
+                        },
                         {
                             thead: 'Email',
                             field: 'email'
@@ -195,15 +347,77 @@ export default {
                 },
                 'user_requests': {
                     name: "Requests",
-                    fields: []
+                    fields: [],
+                    data_template: [
+                        {
+                            thead: 'S.NO#'
+                        },
+                        {
+                            thead: 'ID',
+                            field: 'id'
+                        },
+                        {
+                            thead: 'Origin',
+                            field: 'orgText'
+                        },
+                        {
+                            thead: 'Destination',
+                            field: 'desText'
+                        },
+                        {
+                            thead: 'Distance Est.',
+                            field: 'disText'
+                        },
+                        {
+                            thead: 'Duration Est.',
+                            field: 'durText'
+                        },
+                        {
+                            thead: 'Floors',
+                            field: 'floors'
+                        },
+                        {
+                            thead: 'Labours',
+                            field: 'labours'
+                        },
+                        {
+                            thead: 'Vechile Required',
+                            field: 'vecType'
+                        },
+                        {
+                            thead: 'Created Time',
+                            field: 'createdAt'
+                        },
+                        {
+                            thead: 'Canceled Time',
+                            field: 'canceledAt'
+                        }
+                    ]
                 },
                 'driver_bids': {
                     name: "Bids",
-                    fields: []
-                },
-                'adda_list': {
-                    name: "Adda",
-                    fields: []
+                    fields: [],
+                    data_template: [
+                        {
+                            thead: 'S.NO#'
+                        },
+                        {
+                            thead: 'Request ID',
+                            field: 'req_id'
+                        },
+                        {
+                            thead: 'Amount',
+                            field: 'amount'
+                        },
+                        {
+                            thead: 'First Bid Time',
+                            field: 'first_bid_time'
+                        },
+                        {
+                            thead: 'Driver ID',
+                            field: 'driver_id'
+                        }
+                    ]
                 }
             },
             sel_table: '',
